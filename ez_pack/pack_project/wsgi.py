394f841cf2f3a805067f6f01b6ff78ae64d081c0
bree parker <breannaparker1991@gmail.com>
2022-05-05 15:14:18 -0600

    django startup

---
+++ ez_pack/pack_project/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ez_pack project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ez_pack.settings')
+
+application = get_wsgi_application()
