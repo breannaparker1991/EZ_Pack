bb942075f6c0d09bad2336949f241cb879f6de6d
bree parker <breannaparker1991@gmail.com>
2022-05-05 18:38:04 -0600

    register

--- ez_pack/pack_app/urls.py
+++ ez_pack/pack_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.login),
+  path('/index', views.index),
+  path('/login', views.login),
+  path('/register',views.register),
+  
 ]
